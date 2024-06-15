78f7169b36e0b6320dd0dc6e411384e3367f9ebf
Flow86 <656249+Flow86@users.noreply.github.com>
2024-06-15 10:51:30 +0200

    Import Mod
    Used Example Mod from StationeersMod as base for this

---
+++ FixWindowGaps.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using StationeersMods.Interface;
+
+using FixWindowGaps.Patches;
+
+namespace FixWindowGaps
+{
+    class FixWindowGaps : ModBehaviour
+    {
+        public override void OnLoaded(ContentHandler contentHandler)
+        {
+            UnityEngine.Debug.Log("FixWindowGaps: Hello!");
+
+            Harmony harmony = new Harmony("FixWindowGapsPatches");
+            harmony.PatchAll(typeof(InventoryWindowPatches));
+
+            UnityEngine.Debug.Log("FixWindowGaps: Done!");
+        }
+    }
+}
---
+++ InventoryWindowPatches.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.UI;
+using HarmonyLib;
+
+namespace FixWindowGaps.Patches
+{
+    internal class InventoryWindowPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(InventoryWindow))]
+        [HarmonyPatch(nameof(InventoryWindow.SetVisible))]
+        public static void SetVisible_Prefix(InventoryWindow __instance, bool isVisble)
+        {
+            // This fixes the phantom window issue. The layout will render all items that are active.
+            // If we mark this game object as inactive, it will no longer be rendered in the layout.
+            // This results in no gaps between open windows.
+            __instance.RectTransform.gameObject.SetActive(isVisble);
+        }
+    }
+}
